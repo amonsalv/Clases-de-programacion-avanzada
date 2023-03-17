345e9a4c09c7a2ac68957a6f494273be0baab304
anamonsalve <anamonsalve96@gmail.com>
2023-03-17 02:01:16 -0500

    ejercicio 4 solucion

--- taller/4toejercicio.js
+++ taller/4toejercicio.js
@@ -5,3 +5,17 @@ negativa en Joules.
 Ejemplo: para la entrada {2,4,-8,5,-6} la cantidad de sables
 defectuosos con energía negativa fue de 2 sables.
 */
+
+function contarSablesDefectuosos(sables) {
+    let contador = 0;
+    for (let i = 0; i < sables.length; i++) {
+        if (sables[i] < 0) {
+            contador++;
+        }
+    }
+    return contador;
+}
+
+const sables = [2, 4, -8, 5, -6];
+const cantidadSablesDefectuosos = contarSablesDefectuosos(sables);
+console.log(cantidadSablesDefectuosos); // 2
