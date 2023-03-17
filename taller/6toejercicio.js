e5115d77399d67fc6c30e0c889718c6c9496748a
anamonsalve <anamonsalve96@gmail.com>
2023-03-17 00:41:11 -0500

    answer to ejercicios 6 y 7

--- taller/6toejercicio.js
+++ taller/6toejercicio.js
@@ -14,10 +14,20 @@ let codigos = [];
 for (let i = 0; i < 20; i++) { //vamos a crear un bucle for para crear aleatoriamente las 20 edades y los 20 codigos, para tenerlos en el arreglo, hacemos la iteraccion entre 1 y 20
     edades.push(Math.floor(Math.random() * 100) + 1);
     codigos.push(Math.floor(Math.random() * 20) + 1);//generamos los numeros aleatorios utilizamos el math random,que devuelve un número aleatorio entre 0 y 1. Multiplicamos ese número por 100 y redondeándolo hacia abajo con la función Math.floor(), podemos obtener un número aleatorio entre 0 y 99, que luego sumamos 1 para obtener un número aleatorio entre 1 y 100. Para los códigos, podemos utilizar la misma técnica, pero generando números aleatorios entre 1 y 20.
-  }
+}
 
-  //para encontrar los mayores de edad en el array, utilizamos la funcion math.max, para que nos devuelva el valor maximo del conjunto de numeros para esto esta el spread ... que nos pasa los valores del arreglo
+//para encontrar los mayores de edad en el array, utilizamos la funcion math.max, para que nos devuelva el valor maximo del conjunto de numeros para esto esta el spread ... que nos pasa los valores del arreglo
 
-  let mayorEdad = Math.max(...edades);
+let mayorEdad = Math.max(...edades);
 
-  //
+
+
+let contador = 0; //Hacemos el contador para el numero de repeticiones de la edad mas alta,
+for (let i = 0; i < edades.length; i++) { //y hacemos un for para recorrer el arreglo
+    if (edades[i] === mayorEdad) {// ademas del que el contador nos cuente las veces que aparece la edad mas alta
+        contador++;
+    }
+}
+
+//e imprimimos todo
+console.log("La edad más alta es " + mayorEdad + " y aparece " + contador + " veces.");
--- taller/7moejercicio.js
+++ taller/7moejercicio.js
@@ -4,3 +4,24 @@ un algoritmo que permita recibir dichos 50 números e indicar
 ¿Cuál fue el más cercano a cero? (Si el número es negativo se
 debe mostrar el resultado del valor absoluto)*/
 
+//definimos un array para los 50 numeros
+
+let numeros = [4, -7, 2, 9, -5, -1, -3, 6, 8, -10, -2, 3, 7, -4, 1, -8, -6, 5, 10, -9, -5, 2, 3, -1, 7, -7, 8, 6, -4, -3, 1, -2, -6, 7, 5, -8, 9, -10, 4, 9, -9, 10, -1, 2, -3, -5, 6, -7, -4, 3];
+
+//definimos una variable para almacenar el número más cercano a cero encontrado hasta ahora.
+
+let masCercano = Infinity;// En este punto, no se ha encontrado ninguno, por lo que la variable se inicializa con un valor muy grande para asegurarnos de que cualquier número que encontremos sea más cercano a cero
+
+//recorremos el array con el bucle for:
+
+for (let i = 0; i < numeros.length; i++) {
+    const distancia = Math.abs(numeros[i] - 0); // calculamos la distancia entre el número actual y cero. Para hacer esto, utilizamos la función Math.abs() para obtener el valor absoluto del número actual, ya que necesitamos la distancia y no la diferencia con cero
+
+    if (distancia < masCercano) {
+        masCercano = distancia; //Si la distancia calculada es menor que la distancia del número más cercano encontrado hasta ahora, actualizamos la variable masCercano con el valor actual
+    }
+}  
+
+//Después de recorrer todo el array, mostramos el número más cercano a cero encontrado. Si es un número negativo, mostramos su valor absoluto
+
+console.log(`El número más cercano a cero es ${masCercano > 0 ? masCercano : -masCercano}`);
